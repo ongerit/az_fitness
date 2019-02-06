86b8aa68fc7136496abae7fddcd14482c875e36b
Mohamed Farih <othmanx23@gmail.com>
2019-02-06 15:59:50 -0500

    starting footer component

---
+++ a2z_fitness/src/components/footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './footer.css'
+
+const Footer = () =>{
+    return(
+        <div className='footer'>
+            <h4>&copy;</h4>
+            <a>instagram</a>
+        </div>
+    )
+}
+
+export default Footer;
--- a2z_fitness/src/pages/index.js
+++ a2z_fitness/src/pages/index.js
@@ -1,8 +1,31 @@
 import React from "react"
 import Header from "../components/header";
+import Footer from "../components/footer"
+import '../components/index.css'
 
 export default () => (
     <div>
         <Header />
+        <div>
+            <section className='section section-a'>
+            <div className='container'>
+                <h1>Sports Specific Training and Individual training</h1>
+                <p>This is the section to highlight the sports section
+                    specific training services I provide.  Also, will describe my 
+                    Individual training services I provide.
+                </p>
+            </div>
+            </section>
+
+            <section className='section section-b'>
+            <div className='container'>
+                <h1>Group classes</h1>
+                <p>section where I will describe my group training
+                    services I provide.
+                </p>
+            </div>
+            </section>
+        </div>
+        <Footer />
     </div>
 ) 
