491d5afcd92c730e0c541e4171cfdc54a416a753
Mohamed Farih <othmanx23@gmail.com>
2019-02-06 22:12:27 -0500

    fixes to footer
    WIll need to make footer responsive and add colors and animation to social media icon. As well as link the icon to his account

--- a2z_fitness/src/components/footer.js
+++ a2z_fitness/src/components/footer.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import './footer.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fab);
 
 const Footer = () =>{
     return(
-        <div className='footer'>
-            <h4>&copy;</h4>
-            <a>instagram</a>
+        <footer className='footer'>
+        <div className='footer-container'>
+            <div className='company'>
+                    <h4>&copy; A2Z Fitness LLC</h4>
+            </div>
+            <div className='social'>
+                <FontAwesomeIcon icon={['fab', 'instagram']}  size='3x' />
+            </div>
         </div>
+        </footer>
     )
 }
 
